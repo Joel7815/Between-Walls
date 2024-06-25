81df06d2f5325c39f8221885fc32027346078034
Joel7815 <101325473+Joel7815@users.noreply.github.com>
2024-06-25 12:05:03 +0200

    Dialogue working

--- BetweenWalls/Helpers/actionable.gd
+++ BetweenWalls/Helpers/actionable.gd
@@ -5,10 +5,6 @@ extends Area2D
 @export var time_cost: int = 10
 @export var player: CharacterBody2D
 
-
 func action(player) -> void:
-	if player.deduct_time_units(time_cost):
-		DialogueManager.show_example_dialogue_balloon(dialogue_ressource, dialogue_start)
-	else:
-		print("Player does not have enough time units to talk to this NPC")
-	
+	DialogueManager.show_example_dialogue_balloon(dialogue_ressource, dialogue_start)
+	player.deduct_time_units(time_cost)
--- BetweenWalls/Scenes/Player/player.gd
+++ BetweenWalls/Scenes/Player/player.gd
@@ -12,10 +12,13 @@ extends CharacterBody2D
 var custom_velocity = Vector2.ZERO
 var input_vector = Vector2.ZERO
 var time_units: int = 20
+var end_day_shown: bool = false
+var pending_time_unit_deduction: int = 0
 signal day_changed
 
 func _ready():
 	print("Player starts with ", time_units, " time units")
+	DialogueManager.connect("dialogue_ended", Callable(self, "_on_dialogue_ended"))
 
 func _physics_process(delta):
 	handle_input()
@@ -40,9 +43,8 @@ func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		var actionables = actionable_finder.get_overlapping_areas()
 		if actionables.size() > 0:
-			actionables[0].action(self) 
+			actionables[0].action(self)
 			return
-	
 
 func update_movement(delta):
 	if input_vector != Vector2.ZERO:
@@ -52,19 +54,22 @@ func update_movement(delta):
 
 	velocity = custom_velocity
 
-func deduct_time_units(amount: int) -> bool:
-	if time_units >= amount:
-		time_units -= amount
+func deduct_time_units(amount: int):
+	pending_time_unit_deduction += amount
+
+func apply_pending_time_unit_deduction():
+	if time_units >= pending_time_unit_deduction:
+		time_units -= pending_time_unit_deduction
 		print("Time units remaining: ", time_units)
-		if time_units == 0:
+		if time_units == 0 and not end_day_shown:
 			show_end_day_dialogue()
-		return true
 	else:
 		print("Not enough time units")
-		return false
+	pending_time_unit_deduction = 0
 
 func reset_time_units():
 	time_units = 20
+	end_day_shown = false
 	print("Time units reset for a new day: ", time_units)
 
 func show_end_day_dialogue():
@@ -72,5 +77,12 @@ func show_end_day_dialogue():
 	if end_day_dialogue_resource:
 		print("End day dialogue resource found")
 		DialogueManager.show_example_dialogue_balloon(end_day_dialogue_resource, end_day_dialogue_start)
+		end_day_shown = true
 	else:
 		print("End day dialogue resource not assigned")
+
+func _on_dialogue_ended(resource: DialogueResource):
+	print("Dialogue ended signal received for resource: ", resource)
+	if resource == end_day_dialogue_resource:
+		reset_time_units()
+	apply_pending_time_unit_deduction()
